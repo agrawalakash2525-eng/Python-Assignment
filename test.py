046eef20dd104232f38b6eff4fee377e2a713dfb
agrawalakash2525-eng <agrawalakash2525@gmail.com>
2026-06-22 13:34:30 +0530

    2nd comits

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("Hello : Welcome to python programming ... : ")
+for i in range(1,10):
+    print(i)
+
+
+ # wap to check string len without len() method
+str1="python"
+c=0
+for i in str1:
+    c+=1
+    print(c)         
